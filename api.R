068e19c727bc9b0e363bff09a9675d8d13575585
dragojatwit <77847866+dragojatwit@users.noreply.github.com>
2024-07-26 12:54:56 -0400

    Update api.R

--- api.R
+++ api.R
@@ -1,8 +1,22 @@
-
-# Load the plumber package
 library(plumber)
 
 #* @apiTitle My First API
+
+#* @filter cors
+function(req, res) {
+  res$setHeader("Access-Control-Allow-Origin", "*")
+  res$setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  res$setHeader("Access-Control-Allow-Headers", "Content-Type")
+  
+  # Handle preflight requests
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$status <- 200
+    return(list())
+  }
+  
+  plumber::forward()
+}
+a
 
 #* Echo back the input
 #* @param msg The message to echo
@@ -11,11 +25,12 @@ function(msg="") {
   list(msg = paste("The message is:", msg))
 }
 
-#* Return the sum of two numbers
-#* @param a The first number
-#* @param b The second number
-#* @get /sum
-function(a, b) {
-  as.numeric(a) + as.numeric(b)
+#* Example data endpoint
+#* @get /data
+function() {
+  data <- data.frame(
+    uri = "spotify:track:26I6RaeZZrIMyGAUwfNCxo"
+  )
+  return(data)
 }
 
